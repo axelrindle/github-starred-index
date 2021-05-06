6768fcf68994e8784f0e7c8c238db0fbacf3d02b
Axel Rindle <mail@axelrindle.de>
2021-05-06 18:57:19 +0200

    Create logger when function is called

--- backend/util/fail.js
+++ backend/util/fail.js
@@ -1,14 +1,13 @@
 // Require modules
 const { Logger } = require('../logger');
 
-const myLogger = new Logger('main');
-
 /**
  * Fails the process with an error message.
  *
  * @param {any} err
  */
 module.exports = (err) => {
+	const myLogger = new Logger('main');
     myLogger.error(err?.message ?? 'An error occured!');
 	console.error(err);
     process.exit(-1);
